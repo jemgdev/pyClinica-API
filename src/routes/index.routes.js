fd40bdee3927645b8b1b813d83bbb80a0645c2b6
Ars-5 <manticora1999@gmail.com>
2021-04-29 13:58:07 -0500

    Avance de las citas medicas 35%

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,8 @@
 const routers = {
     specialty: require('./specialty.routes'),
     patient: require('./patient.routes'),
-    administrator: require('./administrator.routes')
+    administrator: require('./administrator.routes'),
+    medicalappointment: require('./medicalappointment.routes')
 }
 
 module.exports = routers
