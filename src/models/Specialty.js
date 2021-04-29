557790c00eb12f2261109855cf47edfff738bc04
Oscar Alonso <oscaralonso.2000@hotmail.com>
2021-04-29 13:47:18 -0500

    Models Campus y especialidad

--- src/models/Specialty.js
+++ src/models/Specialty.js
@@ -1,12 +1,13 @@
 const { Schema, model } = require('mongoose')
 
 const specialtySchema = new Schema({
-    name: { type: String, unique: true },
-    availableHours: { type: Date },
-    campus: {
-        type: Schema.Types.ObjectId,
-        ref: 'campus'
-    }
+    name: { type: String},
+    availableHours: { type: String },
+    campus: {type: Schema.Types.ObjectId},
+    doctors: [{ 
+        type:Schema.Types.ObjectId,
+        ref: 'doctor'
+    }]
 }, {
     versionKey: false,
     timestamps: true
