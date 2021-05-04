7042169db0d983189271ae15bc809d16f3560db4
EnzoVM <enzogvm21@gmail.com>
2021-05-04 13:14:36 -0500

    Actualizacion del controlador Doctor

--- src/controllers/DoctorController.js
+++ src/controllers/DoctorController.js
@@ -40,23 +40,29 @@ DoctorController.listDoctors = async (req, res) => {
 }
 
 DoctorController.deleteDoctors = async (req, res) => {
-    const {idDoctor} = req.params
+    const { idDoctor } = req.params
 
-    const doctorDeleted = await Doctor.findByIdAndDelete(idDoctor)
+    try {
+        const doctorDeleted = await Doctor.findByIdAndDelete(idDoctor)
 
-    res.status(201).json({
-        message: 'Doctor deleted',
-        doctorDeleted
-    })
+        res.status(201).json({
+            message: 'Doctor deleted',
+            doctorDeleted
+        })
+    } catch (error) {
+        res.status(201).json({
+            message: 'Doctor has not been deleted'
+        })
+    }
 }
 
 //Actualizar los datos del Doctor
 DoctorController.updateDoctorById = async (req, res) => {
-    const {name, surname_p, surname_m, email, password, phone, dni, age} = req.body
+    const { name, surname_p, surname_m, email, password, phone, dni, age } = req.body
 
-    const {idDoctor} = req.params
+    const { idDoctor } = req.params
 
-    try{
+    try {
         const doctorUpdated = await Doctor.findByIdAndUpdate(idDoctor, {
             name,
             surname_p,
@@ -66,13 +72,13 @@ DoctorController.updateDoctorById = async (req, res) => {
             phone,
             dni,
             age
-        },{
+        }, {
             new: true
         })
 
         res.status(201).json(doctorUpdated)
 
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
             message: 'There was an error in the user update'
         })
@@ -82,38 +88,38 @@ DoctorController.updateDoctorById = async (req, res) => {
 
 //Logueo del Doctor
 DoctorController.login = async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body
 
     const doctorFound = await Doctor.find({ email })
-    
-    if(doctorFound){
-        try{
-            if(await Doctor.login(password, doctorFound[0].password)){
-               
-                const token = await jwt.sign({id: doctorFound[0]._id}, process.env.SECRET_KEY, {
+
+    if (doctorFound) {
+        try {
+            if (await Doctor.login(password, doctorFound[0].password)) {
+
+                const token = await jwt.sign({ id: doctorFound[0]._id }, process.env.SECRET_KEY, {
 
                     expiresIn: 60 * 60 * 24
 
                 })
-                
+
 
                 res.status(200).json({
                     message: 'token created',
                     token
                 })
-                
-            }else{
+
+            } else {
 
                 res.status(200).json({
                     message: 'token has not been created'
                 })
             }
 
-        }catch(error){
-       
+        } catch (error) {
+
         }
 
-    }else{
+    } else {
         res.status(404).json({
             error: 'Email is not registered'
         })
@@ -124,13 +130,13 @@ DoctorController.login = async (req, res) => {
 //Listados de Doctores por especialidad
 DoctorController.listDoctorBySpecialty = async (req, res) => {
 
-    const {idSpecialty} = req.params
+    const { idSpecialty } = req.params
 
     const specialtyFound = await Specialty.aggregate(
         [
             {
                 $match: {
-                    _id : mongoose.Types.ObjectId(idSpecialty)
+                    _id: mongoose.Types.ObjectId(idSpecialty)
                 }
             },
             {
@@ -139,7 +145,7 @@ DoctorController.listDoctorBySpecialty = async (req, res) => {
                     localField: 'doctors', //Atributo de la collecion Specialty que hara el match
                     foreignField: '_id', //Id de la collections doctors que hara match
                     as: 'doctors' //nombre del nuevo atributo(arreglo)
-                }    
+                }
             },
             {
                 $unwind: '$doctors'  //separar los doctores en diferentes objetos segun la cantidad de matches
@@ -147,10 +153,10 @@ DoctorController.listDoctorBySpecialty = async (req, res) => {
             {
                 //Elegir que atributos mostrar
                 $project: {
-                    name: '$doctors.name', 
-                    avatar: '$doctors.avatar', 
-                    specialty: '$name' 
-                }    
+                    name: '$doctors.name',
+                    avatar: '$doctors.avatar',
+                    specialty: '$name'
+                }
             }
         ]
     )
