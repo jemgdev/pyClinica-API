d3dcc3f2f9e81946c3beb4597dee4d3c0c4a65cb
josueemg <josueomega355@gmail.com>
2021-05-06 15:07:41 -0500

    manejador de errores de doctor agregado

--- src/controllers/DoctorController.js
+++ src/controllers/DoctorController.js
@@ -2,11 +2,44 @@ const Doctor = require('../models/Doctor')
 const jwt = require('jsonwebtoken')
 const DoctorController = {}
 const Specialty = require('../models/Specialty')
-const { Mongoose } = require('mongoose')
 const mongoose = require('mongoose')
+
+const handleErrors = (error) => {
+
+    console.log(error.message, error.code)
+
+    let errors = { name: '', surname_p: '', surname_m: '', email: '', password: '' }
+
+    if (error.code === 11000) {
+        errors.email = 'Email already exists'
+    }
+
+    if (error.errors.name) {
+        errors.name = 'You must to have a name'
+    }
+
+    if (error.errors.surname_p) {
+        errors.surname_p = 'You must to have a fatherLastName'
+    }
+
+    if (error.errors.surname_m) {
+        errors.surname_m = 'You must to have a motherLastName'
+    }
+
+    if (error.errors.email) {
+        errors.email = 'Wrong email'
+    }
+
+    if (error.message === 'Password is wrong') {
+        errors.password = 'Password is wrong'
+    }
+
+    return errors
+}
 
 //Insertar un Doctor
 DoctorController.insertDoctor = async (req, res) => {
+
     const { name, surname_p, surname_m, email, password, phone, dni, age, gender, specialty } = req.body
     const doctorSchema = new Doctor({
         name,
@@ -22,13 +55,15 @@ DoctorController.insertDoctor = async (req, res) => {
     })
 
     try {
+
         const doctorCreate = await doctorSchema.save();
         
         const specialtyFound = await Specialty.findByIdAndUpdate(doctorCreate.specialty, {
+            
             $addToSet:{
                 doctors: doctorCreate._id
             }
-        },{
+        }, {
             new: true
         })
 
@@ -37,7 +72,10 @@ DoctorController.insertDoctor = async (req, res) => {
             specialtyFound
         })
     } catch (error) {
-        console.log(error)
+
+        res.status(401).json({
+            error: handleErrors(error)
+        })
     }
 }
 
@@ -52,9 +90,11 @@ DoctorController.listDoctors = async (req, res) => {
 }
 
 DoctorController.deleteDoctors = async (req, res) => {
+
     const { idDoctor } = req.params
 
     try {
+
         const doctorDeleted = await Doctor.findByIdAndDelete(idDoctor)
 
         const specialtyFound = await Specialty.findByIdAndUpdate(doctorDeleted.specialty, {
@@ -71,6 +111,7 @@ DoctorController.deleteDoctors = async (req, res) => {
             specialtyFound
         })
     } catch (error) {
+
         res.status(201).json({
             message: 'Doctor has not been deleted'
         })
@@ -79,6 +120,7 @@ DoctorController.deleteDoctors = async (req, res) => {
 
 //Actualizar los datos del Doctor
 DoctorController.updateDoctorById = async (req, res) => {
+
     const { name, surname_p, surname_m, email, password, phone, dni, age } = req.body
 
     const { idDoctor } = req.params
@@ -109,6 +151,7 @@ DoctorController.updateDoctorById = async (req, res) => {
 
 //Logueo del Doctor
 DoctorController.login = async (req, res) => {
+
     const { email, password } = req.body
 
     const doctorFound = await Doctor.find({ email })
@@ -141,6 +184,7 @@ DoctorController.login = async (req, res) => {
         }
 
     } else {
+
         res.status(404).json({
             error: 'Email is not registered'
         })
@@ -186,7 +230,6 @@ DoctorController.listDoctorBySpecialty = async (req, res) => {
         message: "Specialty found",
         specialtyFound
     })
-
 }
 
 module.exports = DoctorController
