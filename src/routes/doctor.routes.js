29b16485ed2c85e02ce77870a0c91e9cfe0907c5
Oscar Alonso <oscaralonso.2000@hotmail.com>
2021-05-07 22:33:41 -0500

    se arreglo tambien las rutas de doctor

--- src/routes/doctor.routes.js
+++ src/routes/doctor.routes.js
@@ -5,12 +5,12 @@ const verifyTokenDoctor = require('../middlewares/TokenVerifyDoctor')
 const verifyToken = require('../middlewares/TokenVerify')
 const router = Router()
 
-router.post('/doctor/insertDoctor',verifyTokenAdministrator, insertDoctor)
+router.post('/doctor/insert-doctor',verifyTokenAdministrator, insertDoctor)
 router.post('/doctor/login', login)
-router.put('/doctor/updateDoctor/:idDoctor', verifyTokenDoctor, updateDoctorById)
-router.get('/doctor/listSpecialty/:idSpecialty', verifyToken, listDoctorBySpecialty)
+router.put('/doctor/update-doctor/:iddoctor', verifyTokenDoctor, updateDoctorById)
+router.get('/doctor/list-specialty/:idspecialty', verifyToken, listDoctorBySpecialty)
 router.get('/doctor/list/patient',verifyToken, listDoctors)
 router.get('/doctor/list/admin',verifyTokenAdministrator,listDoctors)
-router.delete('/doctor/delete/:idDoctor',verifyTokenAdministrator, deleteDoctors)
+router.delete('/doctor/delete/:iddoctor',verifyTokenAdministrator, deleteDoctors)
 
 module.exports = router
