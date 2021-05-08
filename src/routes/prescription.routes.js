66b141f348ff48279e32b04e846f23a85bbf8365
Oscar Alonso <oscaralonso.2000@hotmail.com>
2021-05-07 21:45:47 -0500

    agregando token a receta médica

--- src/routes/prescription.routes.js
+++ src/routes/prescription.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
+const verifyTokenDoctor = require('../middlewares/TokenVerifyDoctor')
+const verifyToken = require('../middlewares/TokenVerify')
 const { insertPrescription , updatePrescription, listPrescriptionByIdDoctor, listPrescriptionByIdPatient} = require('../controllers/PrescriptionController')
 const router = Router()
 
-router.post('/prescription/insert', insertPrescription)
-router.put('/prescription/update/:id-prescription', updatePrescription)
-router.get('/prescription/listprescriptions/doctor/:id-doctor', listPrescriptionByIdDoctor)
-router.get('/prescription/listprescriptions/patient/:id-patient', listPrescriptionByIdPatient)
+router.post('/prescription/insert', verifyTokenDoctor,insertPrescription)
+router.put('/prescription/update/:id-prescription', verifyTokenDoctor,updatePrescription)
+router.get('/prescription/listprescriptions/doctor/:id-doctor', verifyTokenDoctor,listPrescriptionByIdDoctor)
+router.get('/prescription/listprescriptions/patient/:id-patient', verifyToken,listPrescriptionByIdPatient)
 
 module.exports = router
